ba55ce029a712da4b92b10858b9ac8c100cacad5
panpenggen <1048232111@qq.com>
2019-03-11 16:27:55 +0800

    修改引用

--- LoveShop/testdata.py
+++ LoveShop/testdata.py
@@ -1,6 +1,8 @@
 from LoveShop.crud.card_crud import *
 from LoveShop.crud.user_crud import *
-from LoveShop.crud.record_crud import *
+from LoveShop.crud.record_crud import add_record
+import datetime
+
 
 add_user('dandan','dandan','12345678')
 
